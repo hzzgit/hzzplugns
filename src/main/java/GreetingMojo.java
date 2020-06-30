c25532f9d1409f978df6dd8f6b00d8cdc0fac365
huzz <605198347@qq.com>
2020-06-30 13:56:23 +0800

    整理代码,kafka封装,redis封装等,基于nocas的web网关转发等

---
+++ src/main/java/GreetingMojo.java
@@ -0,0 +1,17 @@
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * Says "Hi" to the user.
+ *
+ */
+@Mojo( name = "sayhi")
+public class GreetingMojo extends AbstractMojo
+{
+    public void execute() throws MojoExecutionException
+    {
+        System.out.println("hello word");
+    }
+}
